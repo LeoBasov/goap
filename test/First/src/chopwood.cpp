c1ec1f82e2abff7ca56308d990f5e3f8584dd20a
Leo Basov <l.basov@sparc-industries.com>
2019-11-26 15:14:25 +0100

    Added ChoWood

---
+++ test/First/src/chopwood.cpp
@@ -0,0 +1,6 @@
+#include "chopwood.h"
+
+ChopWood::ChopWood()
+{
+
+}
---
+++ test/First/src/chopwood.h
@@ -0,0 +1,11 @@
+#ifndef CHOPWOOD_H
+#define CHOPWOOD_H
+
+
+class ChopWood
+{
+public:
+    ChopWood();
+};
+
+#endif // CHOPWOOD_H
