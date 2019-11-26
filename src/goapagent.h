36882e61ca658d36b08781de3b4b58cf6cad169e
Leo Basov <l.basov@sparc-industries.com>
2019-11-26 15:05:49 +0100

    Fixed input

--- src/goapagent.h
+++ src/goapagent.h
@@ -13,11 +13,11 @@ class GoapAgent : public std::enable_shared_from_this<GoapAgent>
 public:
     GoapAgent();
 
-    void set_up(std::shared_ptr<IGoap> data_provider
-                ,std::vector<std::shared_ptr<GoapAction>> available_actions
-                ,std::shared_ptr<FiniteState> idle_state
-                ,std::shared_ptr<FiniteState> move_to_state
-                ,std::shared_ptr<FiniteState> perform_action_state);
+    void set_up(const std::shared_ptr<IGoap>& data_provider
+                ,const std::vector<std::shared_ptr<GoapAction>>& available_actions
+                ,const std::shared_ptr<FiniteState>& idle_state
+                ,const std::shared_ptr<FiniteState>& move_to_state
+                ,const std::shared_ptr<FiniteState>& perform_action_state);
     void update();
 
     std::shared_ptr<IGoap> data_provider;
